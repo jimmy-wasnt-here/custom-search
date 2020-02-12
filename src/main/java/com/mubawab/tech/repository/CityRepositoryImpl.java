26da2cc2867d3528f51278b731ad7a95f155b9b4
Jaime Torrejón Velásquez <jaime.torrejon@protonmail.com>
2020-02-12 13:01:21 +0100

    Revert previous changes

--- src/main/java/com/mubawab/tech/repository/CityRepositoryImpl.java
+++ src/main/java/com/mubawab/tech/repository/CityRepositoryImpl.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class CityRepositoryExtendedImpl implements CityRepositoryExtended {
+public class CityRepositoryImpl implements CityRepositoryExtended {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
--- src/main/java/com/mubawab/tech/repository/TownRepositoryImpl.java
+++ src/main/java/com/mubawab/tech/repository/TownRepositoryImpl.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class TownRepositoryExtendedImpl implements TownRepositoryExtended {
+public class TownRepositoryImpl implements TownRepositoryExtended {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
