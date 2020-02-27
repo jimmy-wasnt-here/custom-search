fd514c5a2b1aaad23211418d162a3c5bfadaf976
Jaime Torrejón Velásquez <jaime.torrejon@protonmail.com>
2020-02-27 23:17:25 +0100

    Tests for next issues

--- src/test/java/com/mubawab/tech/ExtraAdTests.java
+++ src/test/java/com/mubawab/tech/ExtraAdTests.java
@@ -34,4 +34,45 @@ public class ExtraAdTests {
 		List<AdDto> searchResult = cityController.findAds(Optional.of(wrongParam));
 		assertTrue(searchResult.isEmpty());
 	}
+
+	@Test
+	public void useSpacesAsParams(){
+		String spaces1 = " ";
+		String spaces2 = "  ";
+		String spaces3 = "   ";
+
+		List<AdDto> searchSpaces1 = cityController.findAds(Optional.of(spaces1));
+		List<AdDto> searchSpaces2 = cityController.findAds(Optional.of(spaces2));
+		List<AdDto> searchSpaces3 = cityController.findAds(Optional.of(spaces3));
+
+		assertTrue(searchSpaces1.isEmpty());
+		assertTrue(searchSpaces2.isEmpty());
+		assertTrue(searchSpaces3.isEmpty());
+	}
+
+	@Test
+	public void useMoreThanOneParamOfSameKind(){
+		String searchTwoTowns = "in DerbFrihaTown or in HayAlfalahTown";
+		String searchTwoSubcategories = "villas or houses";
+		String searchTwoCities = "of marrakech or rabat";
+
+		List<AdDto> searchTowns = cityController.findAds(Optional.of(searchTwoTowns));
+		List<AdDto> searchSubcategories = cityController.findAds(Optional.of(searchTwoSubcategories));
+		List<AdDto> searchCities = cityController.findAds(Optional.of(searchTwoCities));
+
+		for (AdDto ad : searchTowns){
+			String townName = ad.getTown();
+			assertTrue(townName.equalsIgnoreCase("DerbFrihaTown") || townName.equalsIgnoreCase("HayAlfalahTown"));
+		}
+
+		for (AdDto ad : searchSubcategories){
+			String subcategoryName = ad.getCategory();
+			assertTrue(subcategoryName.equalsIgnoreCase("villas") || subcategoryName.equalsIgnoreCase("houses"));
+		}
+
+		for (AdDto ad : searchCities){
+			String cityName = ad.getCity();
+			assertTrue(cityName.equalsIgnoreCase("marrakech") || cityName.equalsIgnoreCase("rabat"));
+		}
+	}
 }
